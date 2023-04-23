10b1bcd5c29998f8ab9ac2090b6968cf3471e70d
hussainsultan <hussainz@gmail.com>
2023-04-23 12:21:35 -0400

    update with latest run.py readme and format

--- README.md
+++ README.md
@@ -1,24 +1,44 @@
-## `tpch-duckdb-bench`
+# `tpch-duckdb-bench`
 A small utility to run TPCH benchmarks with DuckDB accompanying my [blog post](https://hussainsultan.com/posts/efficient-duckdb). 
 
 ## Example Results
 I have an example result set in `results/consolidated.csv` using TPCH data at SF100, SF500 and SF1000 [desktop machine](https://pcpartpicker.com/user/Chemisist/saved/#view=JxrNP6).
 
-### Prepare Data
-The following script will download the data and parse it into parquet files
+## Prepare Data
+I used the [tpch docker](https://github.com/databloom-ai/TPCH-Docker) container to prepare the text at appropriate scale factor. For example, for SF100:
 
-### Run
+```
+❯ docker run -v `pwd`/data:/data -it --rm ghcr.io/databloom-ai/tpch-docker:main -vf -s 100 
+```
+To convert to parquet, I used `convert_to_parquet.py`
 
+## Fetch TPC-H queries
+Before we can run the benchmarks, we fetch the queries from the `ibis/tpc-queries` repo:
+```
+git submodule update --init --recursive
+```
+## Run
+The `run.py` cli will run per scale factor (datadir):
 ```
 ❯ python run.py --help
 Usage: run.py [OPTIONS]
+
 Options:
+  --repeat INTEGER                Number of benchmark runs  [default: 1]
+  --comment TEXT                  helpful comments to be added to benchmark
+                                  output
+  --threads TEXT                  comma seperated list of threads to run to
+                                  run  [default: 8]
+  --queries TEXT                  comma seperated list of questions to run
+                                  [default: h01,h02,h03,h04,h05,h06,h07,h08,h0
+                                  9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h2
+                                  0,h21,h22]
+  --engines TEXT                  comma seperated list of datadirs to run e.g.
+                                  duckdb, polars  [default: duckdb]
   --powermetrics / --no-powermetrics
                                   Flag to get cpu and power metrics on OSX
                                   [default: no-powermetrics]
-  --threads TEXT                  comma seperated list of threads to run e.g.
-                                  2,4,8  [default: 8]
-  --datadir TEXT                  [default: data]
+  --datadir TEXT                  comma seperated list of datadirs to run e.g.
+                                  2,4,8  [default: data]
   --help                          Show this message and exit.
 ```
-
--- convert_to_parquet.py
+++ convert_to_parquet.py
@@ -1,110 +1,122 @@
-import duckdb
-import pyarrow.parquet as pq
-import pyarrow.compute as pac
-import pyarrow.csv as pc
-import pyarrow as pa
 from pathlib import Path
 
+import duckdb
+import pyarrow as pa
+import pyarrow.compute as pac
+import pyarrow.csv as pc
+import pyarrow.parquet as pq
 
-tables =["part", "partsupp", "region", "supplier", "lineitem", "orders", "customer", "nation"]
+tables = [
+    "part",
+    "partsupp",
+    "region",
+    "supplier",
+    "lineitem",
+    "orders",
+    "customer",
+    "nation",
+]
 
 
-PARTITIONS = {'orders': ["o_orderdate"], "lineitem": ["partition_col"] }
+PARTITIONS = {"orders": ["o_orderdate"], "lineitem": ["partition_col"]}
 
 
 def get_tpch_schema_duckdb():
     schema = {
-              "part": {
-                  "p_partkey" : "BIGINT",
-                  "p_name" : "VARCHAR",
-                  "p_mfgr": "VARCHAR",
-                  "p_brand": "VARCHAR",
-                  "p_type" : "VARCHAR",
-                  "p_size": "BIGINT",
-                  "p_container": "VARCHAR",
-                  "p_retailprice": "DOUBLE",
-                  "p_comment": "VARCHAR",
-                  },
-              "supplier": {
-                  "s_suppkey": "BIGINT",
-                  "s_name": "VARCHAR",
-                  "s_address": "VARCHAR",
-                  "s_nationkey": "BIGINT",
-                  "s_phone": "VARCHAR",
-                  "s_acctbal": "DOUBLE",
-                  "s_comment": "VARCHAR"
-                  },
-              "partsupp": {
-                  "ps_partkey": "BIGINT",
-                  "ps_suppkey":"BIGINT",
-                  "ps_availqty": "BIGINT",
-                  "ps_supplycost": "DOUBLE",
-                  "ps_comment": "VARCHAR"
-                  },
-              "customer": {
-                  "c_custkey": "BIGINT",
-                  "c_name": "VARCHAR",
-                  "c_address": "VARCHAR",
-                  "c_nationkey": "BIGINT",
-                  "c_phone": "VARCHAR",
-                  "c_acctbal": "DOUBLE",
-                  "c_mktsegment": "VARCHAR",
-                  "c_comment": "VARCHAR",
-                  },
-              "orders": {
-                  "o_orderkey": "BIGINT",
-                  "o_custkey": "BIGINT",
-                  "o_orderstatus": "VARCHAR",
-                  "o_totalprice": "DOUBLE",
-                  "o_orderdate": "DATE",
-                  "o_orderpriority": "VARCHAR",
-                  "o_clerk": "VARCHAR",
-                  "o_shippriority": "BIGINT",
-                  "o_comment": "VARCHAR",
-                  "o_null": "VARCHAR"
-                  },
-              "lineitem": {
-                  "l_orderkey": "BIGINT",
-                  "l_partkey": "BIGINT",
-                  "l_suppkey": "BIGINT",
-                  "l_linenumber": "BIGINT",
-                  "l_quantity": "DOUBLE",
-                  "l_extendedprice": "DOUBLE",
-                  "l_discount": "DOUBLE",
-                  "l_tax": "DOUBLE",
-                  "l_returnflag": "VARCHAR",
-                  "l_linestatus": "VARCHAR",
-                  "l_shipdate": "DATE",
-                  "l_commitdate": "DATE",
-                  "l_receiptdate": "DATE",
-                  "l_shipinstruct": "VARCHAR",
-                  "l_shipmode": "VARCHAR",
-                  "l_comment": "VARCHAR",
-                  },
-              "nation": {
-                  "n_nationkey": "BIGINT",
-                  "n_name": "VARCHAR",
-                  "n_regionkey": "BIGINT",
-                  "n_comment": "VARCHAR",
-                },
-              "region": {
-                      "r_regionkey": "BIGINT",
-                      "r_name": "VARCHAR",
-                      "r_comment": "VARCHAR",
-                      "r_null": "VARCHAR"
-                      }
-              }
+        "part": {
+            "p_partkey": "BIGINT",
+            "p_name": "VARCHAR",
+            "p_mfgr": "VARCHAR",
+            "p_brand": "VARCHAR",
+            "p_type": "VARCHAR",
+            "p_size": "BIGINT",
+            "p_container": "VARCHAR",
+            "p_retailprice": "DOUBLE",
+            "p_comment": "VARCHAR",
+        },
+        "supplier": {
+            "s_suppkey": "BIGINT",
+            "s_name": "VARCHAR",
+            "s_address": "VARCHAR",
+            "s_nationkey": "BIGINT",
+            "s_phone": "VARCHAR",
+            "s_acctbal": "DOUBLE",
+            "s_comment": "VARCHAR",
+        },
+        "partsupp": {
+            "ps_partkey": "BIGINT",
+            "ps_suppkey": "BIGINT",
+            "ps_availqty": "BIGINT",
+            "ps_supplycost": "DOUBLE",
+            "ps_comment": "VARCHAR",
+        },
+        "customer": {
+            "c_custkey": "BIGINT",
+            "c_name": "VARCHAR",
+            "c_address": "VARCHAR",
+            "c_nationkey": "BIGINT",
+            "c_phone": "VARCHAR",
+            "c_acctbal": "DOUBLE",
+            "c_mktsegment": "VARCHAR",
+            "c_comment": "VARCHAR",
+        },
+        "orders": {
+            "o_orderkey": "BIGINT",
+            "o_custkey": "BIGINT",
+            "o_orderstatus": "VARCHAR",
+            "o_totalprice": "DOUBLE",
+            "o_orderdate": "DATE",
+            "o_orderpriority": "VARCHAR",
+            "o_clerk": "VARCHAR",
+            "o_shippriority": "BIGINT",
+            "o_comment": "VARCHAR",
+            "o_null": "VARCHAR",
+        },
+        "lineitem": {
+            "l_orderkey": "BIGINT",
+            "l_partkey": "BIGINT",
+            "l_suppkey": "BIGINT",
+            "l_linenumber": "BIGINT",
+            "l_quantity": "DOUBLE",
+            "l_extendedprice": "DOUBLE",
+            "l_discount": "DOUBLE",
+            "l_tax": "DOUBLE",
+            "l_returnflag": "VARCHAR",
+            "l_linestatus": "VARCHAR",
+            "l_shipdate": "DATE",
+            "l_commitdate": "DATE",
+            "l_receiptdate": "DATE",
+            "l_shipinstruct": "VARCHAR",
+            "l_shipmode": "VARCHAR",
+            "l_comment": "VARCHAR",
+        },
+        "nation": {
+            "n_nationkey": "BIGINT",
+            "n_name": "VARCHAR",
+            "n_regionkey": "BIGINT",
+            "n_comment": "VARCHAR",
+        },
+        "region": {
+            "r_regionkey": "BIGINT",
+            "r_name": "VARCHAR",
+            "r_comment": "VARCHAR",
+            "r_null": "VARCHAR",
+        },
+    }
     return schema
+
 
 def convert_to_parquet():
     schema = get_tpch_schema_duckdb()
     con = duckdb.connect()
     for t in tables:
         f = Path(f"sf500/{t}.tbl")
-        con.sql(f"create view {t} as select * from read_csv('{f}', delim='|', columns={schema[t]});")
+        con.sql(
+            f"create view {t} as select * from read_csv('{f}', delim='|', columns={schema[t]});"
+        )
         directory_name = f.name.split(".tbl")[0]
         con.sql(f"COPY {t} to '{f.parent/directory_name}.parquet' (FORMAT PARQUET);")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     convert_to_parquet()
